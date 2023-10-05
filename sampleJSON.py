5fcbf7ef93dfdfb446bb13f9e4e00a3adebd9f33
NetAcad DEVASC <changeme@example.com>
2023-10-05 13:07:45 +0000

    new

--- sampleJSON.py
+++ sampleJSON.py
@@ -1,15 +1,12 @@
 import json
 
-
-
-
-
 with open('sample.json', 'r') as json_file:
-
     json_data = json.load(json_file)
     #print(json_data)
-    #print(json.dumps(json_data, indent=4))
+    #print(json.dumps(json_data, indent= 4))
 
-test = json_data['course']
+for courses in json_data['certifications']:
+    print(courses['courses'])
 
-print(test)
+
+#CABARIO
